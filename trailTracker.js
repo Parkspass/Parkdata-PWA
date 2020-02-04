474630cf150bef9e87042af4b10ed1e7c7e68fe6
rory-wagner <rory.wagner@dixiesuccess.org>
2020-02-04 16:27:18 -0700

    Took out some useless code

--- trailTracker.js
+++ trailTracker.js
@@ -216,28 +216,7 @@ Vue.component("report", {
 
     //used to find location:
     mounted(){
-
-        // function success(pos) {
-        //     var crd = pos.coords;
-
-        //     const status = document.querySelector('#status');
-        //     const latitude  = crd.latitude;
-        //     const longitude = crd.longitude;
-
-        //     console.log('Your current position is:');
-        //     console.log(`Latitude : ${latitude}`);
-        //     console.log(`Longitude: ${longitude}`);
-        //     console.log(`More or less ${crd.accuracy} meters.`);
-
-        //     status.textContent = "";
-
-        //     this.latitude = latitude;
-        //     this.longitude = longitude;
-
-        // }
-
         
-
         function error() {
             status.textContent = 'Unable to retrieve your location';
         }
@@ -366,14 +345,14 @@ const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
 
-  addBtn.addEventListener('click', (e) => {
+    addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
     // Show the prompt
@@ -381,11 +360,11 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
+            console.log('User accepted the A2HS prompt');
         } else {
-          console.log('User dismissed the A2HS prompt');
+            console.log('User dismissed the A2HS prompt');
         }
         deferredPrompt = null;
-      });
-  });
+        });
+    });
 });
