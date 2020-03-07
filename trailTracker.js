021874a114262c65027919589a5c667657c86bcd
cade.gardner <d00299297@dmail.dixie.edu>
2020-03-07 11:45:49 -0700

    fixed select labels

--- trailTracker.js
+++ trailTracker.js
@@ -34,7 +34,7 @@ Vue.component("report", {
         <p>
             <div class="inputline">
                 <img id="trail_button" src="pics/trail_button.svg"><select id="location" v-model="location">
-                    <option :value="null" disabled>Select a Trail...</option>
+                    <option :value="null" disabled>Trail or Segment Name</option>
                     <option
                         v-for="(trail, index) in trails"
                         :key="index">{{ trail }}</option>
@@ -59,7 +59,7 @@ Vue.component("report", {
         <p>
             <div class="inputline">
                 <img id="weather_button" src="pics/weather_button.svg"><select id="weatherOptions" v-model="selectedWeatherOption">
-                <option :value="null" disabled>Weather...</option>
+                <option :value="null" disabled>Weather</option>
                 <option
                     v-for="(weather, index) in weatherOptions"
                     :key="index">{{ weather }}</option>
@@ -78,7 +78,7 @@ Vue.component("report", {
             <div class="inputline">
                 <img id="visitation_button" src="pics/visitation_button.svg">
                 <select id="visitationOptions" v-model="selectedVisitationOption">
-                    <option :value="null" disabled>Visitation Status...</option>
+                    <option :value="null" disabled>Visitation Status</option>
                     <option
                         v-for="(visitation, index) in visitationOptions"
                         :key="index">{{ visitation }}</option>
@@ -90,7 +90,7 @@ Vue.component("report", {
             <div class="inputline">
                 <img id="trail_status_button" src="pics/trail_status_button.svg">
                 <select id="trailStatusOptions" v-model="selectedTrailStatusOption">
-                    <option :value="null" disabled>Trail Status...</option>
+                    <option :value="null" disabled>Trail Status</option>
                     <option
                         v-for="(trailStatus, index) in trailStatusOptions"
                         :key="index">{{ trailStatus }}</option>
@@ -102,7 +102,7 @@ Vue.component("report", {
             <div class="inputline">
                 <img id="trail_conditions_button" src="pics/trail_conditions_button.svg">
                 <select id="trailConditionsOptions" v-model="selectedTrailConditionsOption">
-                    <option :value="null" disabled>Trail Condition...</option>
+                    <option :value="null" disabled>Trail Conditions</option>
                     <option
                         v-for="(trailConditions, index) in trailConditionsOptions"
                         :key="index">{{ trailConditions }}</option>
